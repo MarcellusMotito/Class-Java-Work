ceb02437fe3a3904c0e6f60225343c948009fce8
MarcellusMotito <mmarcellm21@gmail.com>
2025-03-03 04:17:10 -0800

    Delete lesson two directory

--- lesson two/IfExample.java
+++
@@ -1,8 +0,0 @@
-public class IfExample{
-    public static void main(String[] args) {
-        int berrieCount = 6;
-        if (berrieCount>5){
-            System.out.println("More than handfull of berries");
-        }     
-    }
-}
--- lesson two/forloopExample.java
+++
@@ -1,8 +0,0 @@
-public class forloopExample {
-    public static void main(String[] args) {
-        for(int count=2; count<10; count++){
-            System.out.println(count);
-        }
-    }
-
-}
