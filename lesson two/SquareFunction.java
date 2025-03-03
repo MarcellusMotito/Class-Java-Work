cc3fda99ede907b6119eacd3d58ba0723cd10f66
MarcellusMotito <mmarcellm21@gmail.com>
2025-03-03 04:15:23 -0800

    Add files via upload

---
+++ lesson two/SquareFunction.java
@@ -0,0 +1,10 @@
+public class SquareFunction {
+    static int square(int number){
+        return number * number;
+    }
+    public static void main(String[] args) {
+        int result=square(8);
+        System.out.println("The square of the number is: " + result);
+    }
+
+}
---
+++ lesson two/dowhileloopExample.java
@@ -0,0 +1,12 @@
+public class dowhileloopExample {
+    public static void main(String[] args) {
+        int number=1;
+
+        do {
+            System.out.println(number);
+            number++;
+        } while (number < 10);
+        
+    }
+
+}
